6a4e3c970988bd50e32caf7f564d837561bb004d
Juan_Martinez <jmartinez304@mail.valenciacollege.edu>
2019-10-21 21:21:45 -0400

    Added comment to AllTest.java file

--- src/AllTests.java
+++ src/AllTests.java
@@ -2,6 +2,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * 
+ * Testing Suite that includes all the tests classes of the
+ * TextAnalyzerWithUITest project.
+ *
+ */
+
 @RunWith(Suite.class)
 @SuiteClasses({ WordFrequencyFinderTest.class, WordTest.class })
 public class AllTests {
